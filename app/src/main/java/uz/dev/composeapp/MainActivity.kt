ad56dbfda51c76cf3dedc9e0f4a8700aa739c12f
Rustam <rustamsmax@gmail.com>
2021-08-26 09:27:48 +0500

    test with 02

--- app/src/main/java/uz/dev/composeapp/MainActivity.kt
+++ app/src/main/java/uz/dev/composeapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -28,11 +30,14 @@ import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.layout.RelocationRequester
+import androidx.compose.ui.layout.onRelocationRequest
 import androidx.compose.ui.layout.relocationRequester
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import kotlinx.coroutines.launch
 import uz.dev.composeapp.ui.theme.ComposeAppTheme
+import kotlin.math.abs
 
 class MainActivity : ComponentActivity() {
 
@@ -40,7 +45,7 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       ComposeAppTheme {
-        BringIntoViewDemo()
+        BringPartOfComposableIntoViewSample()
       }
     }
   }
@@ -49,40 +54,45 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun BringIntoViewDemo() {
-  val greenRequester = remember { RelocationRequester() }
-  val redRequester = remember { RelocationRequester() }
-  val coroutineScope = rememberCoroutineScope()
-  Column {
-    Column(Modifier.requiredHeight(100.dp).verticalScroll(rememberScrollState())) {
-      Row(Modifier.width(300.dp).horizontalScroll(rememberScrollState())) {
-        Box(Modifier.background(Blue).size(100.dp))
-        Box(Modifier.background(Green).size(100.dp).relocationRequester(greenRequester))
-        Box(Modifier.background(Yellow).size(100.dp))
-        Box(Modifier.background(Magenta).size(100.dp))
-        Box(Modifier.background(Gray).size(100.dp))
-        Box(Modifier.background(Black).size(100.dp))
+fun BringRectangleIntoViewDemo() {
+  with(LocalDensity.current) {
+    val relocationRequester = remember { RelocationRequester() }
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+      Text(
+        "This is a scrollable Box. Drag to scroll the Circle into view or click the " +
+              "button to bring the circle into view."
+      )
+      Box(
+        Modifier
+          .border(2.dp, Black)
+          .size(500f.toDp())
+          .horizontalScrollWithRelocation(rememberScrollState())
+      ) {
+        Canvas(
+          Modifier
+            .size(1500f.toDp(), 500f.toDp())
+            .relocationRequester(relocationRequester)
+        ) {
+          drawCircle(color = Red, radius = 250f, center = Offset(750f, 250f))
+        }
       }
-      Row(Modifier.width(300.dp).horizontalScroll(rememberScrollState())) {
-        Box(Modifier.background(Black).size(100.dp))
-        Box(Modifier.background(Cyan).size(100.dp))
-        Box(Modifier.background(DarkGray).size(100.dp))
-        Box(Modifier.background(White).size(100.dp))
-        Box(Modifier.background(Red).size(100.dp).relocationRequester(redRequester))
-        Box(Modifier.background(LightGray).size(100.dp))
+      Button(
+        onClick = {
+          val circleCoordinates = Rect(500f, 0f, 1000f, 500f)
+          coroutineScope.launch {
+            relocationRequester.bringIntoView(circleCoordinates)
+          }
+        }
+      ) {
+        Text("Bring circle into View")
       }
-    }
-    Button(onClick = { coroutineScope.launch { greenRequester.bringIntoView() } }) {
-      Text("Bring Green box into view")
-    }
-    Button(onClick = { coroutineScope.launch { redRequester.bringIntoView() } }) {
-      Text("Bring Red box into view")
     }
   }
 }
 
 
-@ExperimentalComposeUiApi
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun BringPartOfComposableIntoViewSample() {
   with(LocalDensity.current) {
@@ -119,4 +129,44 @@ fun BringPartOfComposableIntoViewSample() {
       }
     }
   }
+}
+
+// This is a helper function that users will have to use since experimental "ui" API cannot be used
+// inside Scrollable, which is ihe "foundation" package. After onRelocationRequest is added
+// to Scrollable, users can use Modifier.horizontalScroll directly.
+@OptIn(ExperimentalComposeUiApi::class)
+private fun Modifier.horizontalScrollWithRelocation(
+  state: ScrollState,
+  enabled: Boolean = true,
+  flingBehavior: FlingBehavior? = null,
+  reverseScrolling: Boolean = false
+): Modifier {
+  return this
+    .onRelocationRequest(
+      onProvideDestination = { rect, layoutCoordinates ->
+        val size = layoutCoordinates.size.toSize()
+        rect.translate(relocationDistance(rect.left, rect.right, size.width), 0f)
+      },
+      onPerformRelocation = { source, destination ->
+        val offset = destination.left - source.left
+        state.animateScrollBy(if (reverseScrolling) -offset else offset)
+      }
+    )
+    .horizontalScroll(state, enabled, flingBehavior, reverseScrolling)
+}
+
+// Calculate the offset needed to bring one of the edges into view. The leadingEdge is the side
+// closest to the origin (For the x-axis this is 'left', for the y-axis this is 'top').
+// The trailing edge is the other side (For the x-axis this is 'right', for the y-axis this is
+// 'bottom').
+private fun relocationDistance(leadingEdge: Float, trailingEdge: Float, parentSize: Float) = when {
+  // If the item is already visible, no need to scroll.
+  leadingEdge >= 0 && trailingEdge <= parentSize -> 0f
+
+  // If the item is visible but larger than the parent, we don't scroll.
+  leadingEdge < 0 && trailingEdge > parentSize -> 0f
+
+  // Find the minimum scroll needed to make one of the edges coincide with the parent's edge.
+  abs(leadingEdge) < abs(trailingEdge - parentSize) -> leadingEdge
+  else -> trailingEdge - parentSize
 }
