fabfc918fc660897625f8126944dfd4f1db8c532
Rustam <rustamsmax@gmail.com>
2021-09-12 22:06:41 +0500

    for Silent829

--- app/src/main/java/uz/dev/composeapp/SilentCode.kt
+++ app/src/main/java/uz/dev/composeapp/SilentCode.kt
@@ -69,6 +69,8 @@ fun AuthenticationContent() {
     val scaffoldState = rememberScaffoldState()
     val scrollState = rememberScrollState()
 
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = {
@@ -77,133 +79,149 @@ fun AuthenticationContent() {
             )
         },
         content = { paddings ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddings)
-                    .padding( horizontal = 16.dp)
-                    .verticalScroll(scrollState),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Spacer(modifier = Modifier.height(100.dp))
-                Text(
-                    text = stringResource(id = R.string.label_greeting),
-                    style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.SemiBold)
-                )
-
-                Text(
-                    modifier = Modifier
-                        .height(height = 48.dp)
-                        .padding(top = 4.dp),
-                    text = stringResource(id = R.string.label_enter_to_continue),
-                    style = MaterialTheme.typography.subtitle1
-                )
-                Spacer(modifier = Modifier.height(40.dp))
-
-//                var visibleArea by remember { mutableStateOf<Rect?>(null) }
+            Box {
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .focusable()
-                        .relocate()
-                       /* .onGloballyPositioned {
+                        .padding(paddings)
+                        .padding(horizontal = 16.dp)
+                        .verticalScroll(scrollState),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(100.dp))
+                    Text(
+                        text = stringResource(id = R.string.label_greeting),
+                        style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.SemiBold)
+                    )
+
+                    Text(
+                        modifier = Modifier
+                            .height(height = 48.dp)
+                            .padding(top = 4.dp),
+                        text = stringResource(id = R.string.label_enter_to_continue),
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                    Spacer(modifier = Modifier.height(40.dp))
+
+//                var visibleArea by remember { mutableStateOf<Rect?>(null) }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .focusable()
+                            .relocate()
+                        /* .onGloballyPositioned {
                             visibleArea = it.size
                                 .toSize()
                                 .toRect()
                         }*/,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    val clearPhoneButton: @Composable (() -> Unit)? =
-                        if (state.phoneNumber.isEmpty()) null
-                        else {
-                            {
-                                IconButton(
-                                    onClick = { state = state.copy(phoneNumber = "") },
-                                    content = { Icon(Icons.Default.Clear, null) }
-                                )
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        val clearPhoneButton: @Composable (() -> Unit)? =
+                            if (state.phoneNumber.isEmpty()) null
+                            else {
+                                {
+                                    IconButton(
+                                        onClick = { state = state.copy(phoneNumber = "") },
+                                        content = { Icon(Icons.Default.Clear, null) }
+                                    )
+                                }
                             }
-                        }
 
-                    TextField(
-                        modifier = Modifier
+                        TextField(
+                            modifier = Modifier
 //                            .relocate(/*visibleArea*/)
-                            .padding(top = 32.dp),
-                        value = state.phoneNumber,
-                        onValueChange = { phoneNumber ->
-                            state = state.copy(phoneNumber = phoneNumber)
-                        },
-                        label = { Text(stringResource(id = R.string.label_phone_number)) },
-                        placeholder = { Text(stringResource(id = R.string.label_phone_number_placeholder)) },
-                        trailingIcon = clearPhoneButton,
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
-                    )
-
-                    Spacer(modifier = Modifier.height(100.dp))
-
-                    TextField(
-                        modifier = Modifier
-//                            .relocate(/*visibleArea*/)
-                            .padding(top = 16.dp),
-                        value = state.password,
-                        onValueChange = { password ->
-                            state = state.copy(password = password)
-                        },
-                        label = { Text(stringResource(id = R.string.label_password)) },
-                        placeholder = { Text(stringResource(id = R.string.label_password_placeholder)) },
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                        visualTransformation = when (state.showPassword) {
-                            true -> VisualTransformation.None
-                            false -> PasswordVisualTransformation()
-                        }
-                    )
-
-                    Button(
-                        modifier = Modifier
-                            .padding(top = 32.dp)
-                            .fillMaxWidth(),
-                        content = { Text(stringResource(id = R.string.label_login)) },
-                        onClick = {
-                            // some logic to sign in
-                        }
-                    )
-
-                    OutlinedButton(
-                        modifier = Modifier
-//								.navigationBarsWithImePadding()
-                            .padding(top = 16.dp),
-                        content = { Text(stringResource(id = R.string.label_login_via_sms)) },
-                        onClick = {
-                            // some login to sign in with sms
-                        }
-                    )
-                    val annotatedRegistrationText = buildAnnotatedString {
-                        append(text = stringResource(id = R.string.label_registration_part_1)).also {
-                            append(text = " ")
-                        }
-                        pushStringAnnotation(
-                            tag = "register_link",
-                            annotation = stringResource(
-                                id = R.string.label_registration_part_tag
-                            )
+                                .padding(top = 32.dp),
+                            value = state.phoneNumber,
+                            onValueChange = { phoneNumber ->
+                                state = state.copy(phoneNumber = phoneNumber)
+                            },
+                            label = { Text(stringResource(id = R.string.label_phone_number)) },
+                            placeholder = { Text(stringResource(id = R.string.label_phone_number_placeholder)) },
+                            trailingIcon = clearPhoneButton,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
                         )
-                        withStyle(style = SpanStyle(color = MaterialTheme.colors.secondary)) {
-                            append(text = stringResource(id = R.string.label_registration_part_2))
-                        }
-                        pop()
-                    }
-                    ClickableText(
-                        modifier = Modifier
-                            .fillMaxWidth()
+
+                        Spacer(modifier = Modifier.height(100.dp))
+
+                        TextField(
+                            modifier = Modifier
+//                            .relocate(/*visibleArea*/)
+                                .padding(top = 16.dp),
+                            value = state.password,
+                            onValueChange = { password ->
+                                state = state.copy(password = password)
+                            },
+                            label = { Text(stringResource(id = R.string.label_password)) },
+                            placeholder = { Text(stringResource(id = R.string.label_password_placeholder)) },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                            visualTransformation = when (state.showPassword) {
+                                true -> VisualTransformation.None
+                                false -> PasswordVisualTransformation()
+                            }
+                        )
+
+                        Button(
+                            modifier = Modifier
+                                .padding(top = 32.dp)
+                                .fillMaxWidth(),
+                            content = { Text(stringResource(id = R.string.label_login)) },
+                            onClick = {
+                                // some logic to sign in
+                                scope.launch {
+                                    snackbarHostState.showSnackbar(message = "Login is not implemented")
+                                }
+                            }
+                        )
+
+                        OutlinedButton(
+                            modifier = Modifier
 //								.navigationBarsWithImePadding()
-                            .padding(top = 16.dp),
-                        text = annotatedRegistrationText,
-                        style = MaterialTheme.typography.body2
-                            .copy(textAlign = TextAlign.Center),
-                        onClick = {
-                            // todo
+                                .padding(top = 16.dp),
+                            content = { Text(stringResource(id = R.string.label_login_via_sms)) },
+                            onClick = {
+                                // some login to sign in with sms
+                                scope.launch {
+                                    snackbarHostState.showSnackbar(message = "Login via sms not implemented")
+                                }
+                            }
+                        )
+                        val annotatedRegistrationText = buildAnnotatedString {
+                            append(text = stringResource(id = R.string.label_registration_part_1)).also {
+                                append(text = " ")
+                            }
+                            pushStringAnnotation(
+                                tag = "register_link",
+                                annotation = stringResource(
+                                    id = R.string.label_registration_part_tag
+                                )
+                            )
+                            withStyle(style = SpanStyle(color = MaterialTheme.colors.secondary)) {
+                                append(text = stringResource(id = R.string.label_registration_part_2))
+                            }
+                            pop()
                         }
+                        ClickableText(
+                            modifier = Modifier
+                                .fillMaxWidth()
+//								.navigationBarsWithImePadding()
+                                .padding(top = 16.dp),
+                            text = annotatedRegistrationText,
+                            style = MaterialTheme.typography.body2
+                                .copy(textAlign = TextAlign.Center),
+                            onClick = {
+                                // todo
+                            }
+                        )
+                    }
+                }
+                SnackbarHost(snackbarHostState){ data ->
+                    Snackbar(
+                        modifier = Modifier
+                            .align(alignment = Alignment.TopCenter),
+                        snackbarData = data
                     )
                 }
+
             }
         },
         bottomBar = {
